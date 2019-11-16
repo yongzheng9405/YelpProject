10d7036b48e3c05002a0e62ec8ecf0816a9a7cc9
yongzheng zhang <yongzheng9405@gmail.com>
2019-11-16 11:17:51 +0800

    Add builder pattern
    User builder pattern to build item object

--- Project/Titan/src/entity/Item.java
+++ Project/Titan/src/entity/Item.java
@@ -6,6 +6,60 @@ import org.json.JSONObject;
 import org.json.JSONException;
 
 public class Item {
+	public static class ItemBuilder {
+		private String itemId;
+		private String name;
+		private double rating;
+		private String address;
+		private double distance;
+		private Set<String> categories;
+		private String imageUrl;
+		private String url;
+		
+		public ItemBuilder setItemId(String itemId) {
+			this.itemId = itemId;
+			return this;
+		}
+		public ItemBuilder setName(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public ItemBuilder setRating(double rating) {
+			this.rating = rating;
+			return this;
+		}
+
+		public ItemBuilder setAddress(String address) {
+			this.address = address;
+			return this;
+		}
+
+		public ItemBuilder setDistance(double distance) {
+			this.distance = distance;
+			return this;
+		}
+
+		public ItemBuilder setCategories(Set<String> categories) {
+			this.categories = categories;
+			return this;
+		}
+
+		public ItemBuilder setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+			return this;
+		}
+
+		public ItemBuilder setUrl(String url) {
+			this.url = url;
+			return this;
+		}
+
+		public Item build() {
+			return new Item(this);
+		}
+
+	}
 	private String itemId;
 	private String name;
 	private double rating;
@@ -15,6 +69,16 @@ public class Item {
 	private String imageUrl;
 	private String url;
 	
+	private Item(ItemBuilder builder) {
+		this.itemId = builder.itemId;
+		this.name = builder.name;
+		this.rating = builder.rating;
+		this.address = builder.address;
+		this.distance = builder.distance;
+		this.categories = builder.categories;
+		this.imageUrl = builder.imageUrl;
+		this.url = builder.url;
+	}
 	public String getItemId() {
 		return itemId;
 	}
@@ -39,6 +103,8 @@ public class Item {
 	public String getUrl() {
 		return url;
 	}
+	
+	
 	
 	public JSONObject toJSONObject() {
 		JSONObject obj = new JSONObject();
